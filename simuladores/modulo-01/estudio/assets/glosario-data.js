2b7437d06db1c5b509a09c42fb5b30fb289cb4af
Sergio Gevatschnaider <132850405+sgevatschnaider@users.noreply.github.com>
2026-07-25 22:27:56 -0300

    Add glossary data loader

---
+++ simuladores/modulo-01/estudio/assets/glosario-data.js
@@ -0,0 +1,6 @@
+window.GLOSSARY_DATA=[];
+document.write('<script src="assets/glosario-data-1.js"><\/script>');
+document.write('<script src="assets/glosario-data-2.js"><\/script>');
+document.write('<script src="assets/glosario-data-3.js"><\/script>');
+document.write('<script src="assets/glosario-data-4.js"><\/script>');
+document.write('<script src="assets/glosario-data-5.js"><\/script>');
