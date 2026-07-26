3babc183716ed1d33fd693adae3771ba53038e44
Sergio Gevatschnaider <132850405+sgevatschnaider@users.noreply.github.com>
2026-07-25 22:28:07 -0300

    Add questions data loader

---
+++ simuladores/modulo-01/estudio/assets/preguntas-data.js
@@ -0,0 +1,4 @@
+window.QA_DATA=[];
+document.write('<script src="assets/preguntas-data-1.js"><\/script>');
+document.write('<script src="assets/preguntas-data-2.js"><\/script>');
+document.write('<script src="assets/preguntas-data-3.js"><\/script>');
